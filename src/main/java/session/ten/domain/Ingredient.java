585ffd3aeddb035419f222e77f5c42c4c24f53ef
Benjamin <github@avalax.de>
2015-02-26 21:38:17 +0100

    Session 10.2: Replace One/Many Distinctions with Meal Composite

--- src/main/java/session/ten/domain/Ingredient.java
+++ src/main/java/session/ten/domain/Ingredient.java
@@ -1,33 +1,33 @@
 package session.ten.domain;
 
 public class Ingredient implements Nutriment {
-	private Long id;
+    private Long id;
     private double gramm;
-	private int kcal;
-	private double fat;
-	private double carbohydrates;
-	private double protein;
-	private double salt;
-	private String name;
+    private int kcal;
+    private double fat;
+    private double carbohydrates;
+    private double protein;
+    private double salt;
+    private String name;
 
-	public Ingredient(String name, int kcal) {
-		this.name = name;
-		this.kcal = kcal;
-		this.gramm = 100;
-		this.fat = 0;
-		this.carbohydrates = 0;
-		this.protein = 0;
-		this.salt = 0;
-	}
+    public Ingredient(String name, int kcal) {
+        this.name = name;
+        this.kcal = kcal;
+        this.gramm = 100;
+        this.fat = 0;
+        this.carbohydrates = 0;
+        this.protein = 0;
+        this.salt = 0;
+    }
 
-	public Ingredient(String name, int kcal, double gramm, double fat, double carbohydrates, double protein, double salt) {
-		this(name, kcal);
+    public Ingredient(String name, int kcal, double gramm, double fat, double carbohydrates, double protein, double salt) {
+        this(name, kcal);
         this.gramm = gramm;
         this.fat = fat;
         this.carbohydrates = carbohydrates;
-		this.protein = protein;
-		this.salt = salt;
-	}
+        this.protein = protein;
+        this.salt = salt;
+    }
 
     public Long getId() {
         return id;
@@ -39,62 +39,62 @@ public class Ingredient implements Nutriment {
 
     @Override
     public int getKcal() {
-		return kcal;
-	}
+        return kcal;
+    }
 
-	public void setKcal(int kcal) {
-		this.kcal = kcal;
-	}
-
-    @Override
-	public double getFat() {
-		return fat;
-	}
-
-	public void setFat(double fat) {
-		this.fat = fat;
-	}
+    public void setKcal(int kcal) {
+        this.kcal = kcal;
+    }
 
     @Override
-	public double getCarbohydrates() {
-		return carbohydrates;
-	}
+    public double getFat() {
+        return fat;
+    }
 
-	public void setCarbohydrates(double carbohydrates) {
-		this.carbohydrates = carbohydrates;
-	}
-
-    @Override
-	public double getProtein() {
-		return protein;
-	}
-
-	public void setProtein(double protein) {
-		this.protein = protein;
-	}
-
-	public double getSalt() {
-		return salt;
-	}
-
-	public void setSalt(double salt) {
-		this.salt = salt;
-	}
+    public void setFat(double fat) {
+        this.fat = fat;
+    }
 
     @Override
-	public String getName() {
-		return name;
-	}
+    public double getCarbohydrates() {
+        return carbohydrates;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setCarbohydrates(double carbohydrates) {
+        this.carbohydrates = carbohydrates;
+    }
 
-	public double getGramm() {
-		return gramm;
-	}
+    @Override
+    public double getProtein() {
+        return protein;
+    }
 
-	public void setGramm(double gramm) {
-		this.gramm = gramm;
-	}
+    public void setProtein(double protein) {
+        this.protein = protein;
+    }
+
+    public double getSalt() {
+        return salt;
+    }
+
+    public void setSalt(double salt) {
+        this.salt = salt;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGramm() {
+        return gramm;
+    }
+
+    public void setGramm(double gramm) {
+        this.gramm = gramm;
+    }
 }
--- src/main/java/session/ten/domain/Meal.java
+++ src/main/java/session/ten/domain/Meal.java
@@ -2,18 +2,68 @@ package session.ten.domain;
 
 import java.util.ArrayList;
 
-public class Meal extends AbstractNutriment {
+public class Meal implements Nutriment {
 	private String name = null;
-
-	public Meal() {
-		 this("new meal");
-	}
 
 	public Meal(String name) {
         nutriments = new ArrayList<Nutriment>();
 		this.name = name;
 	}
 
+    protected Long id;
+    protected ArrayList<Nutriment> nutriments;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void addNutriment(Nutriment nutriment) {
+        nutriments.add(nutriment);
+    }
+
+    public int getKcal() {
+        int kcal = 0;
+        for (Nutriment nutriment : nutriments) {
+            kcal += nutriment.getKcal();
+        }
+        return kcal;
+    }
+
+    public double getFat() {
+        double fat = 0;
+        for (Nutriment nutriment : nutriments) {
+            fat += nutriment.getFat();
+        }
+        return fat;
+    }
+
+    public double getCarbohydrates() {
+        double carbohydrates = 0;
+        for (Nutriment nutriment : nutriments) {
+            carbohydrates += nutriment.getCarbohydrates();
+        }
+        return carbohydrates;
+    }
+
+    public double getProtein() {
+        double protein = 0;
+        for (Nutriment nutriment : nutriments) {
+            protein += nutriment.getProtein();
+        }
+        return protein;
+    }
+
+    public double getSalt() {
+        double salt = 0;
+        for (Nutriment nutriment : nutriments) {
+            salt += nutriment.getSalt();
+        }
+        return salt;
+    }
 	public void setName(String name) {
 		this.name = name;
 	}
